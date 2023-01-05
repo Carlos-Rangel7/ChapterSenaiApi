c29325b8c7cd30daeddeaa7d208f621a57adb7a9
Carlos Rangel <croi7726@gmail.com>
2023-01-05 20:21:43 -0300

    UC17 Encontro Remoto 1

---
+++ ChapterSenai/Contexts/Sqlcontext.cs
@@ -0,0 +1,25 @@
+﻿using ChapterSenai.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChapterSenai.Contexts
+{
+    public class Sqlcontext : DbContext
+    {
+        public Sqlcontext() { }
+        public Sqlcontext(DbContextOptions<Sqlcontext> options) : base(options) { }
+        // vamos utilizar esse método para configurar o banco de dados
+        protected override void
+        OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // cada provedor tem sua sintaxe para especificação
+                //optionsBuilder.UseSqlServer("Data Source = DESKTOP-JB6Q0DG\\MSSSQLSERVER15; initial catalog = Chapter; user id = sa; password  = 123")
+                optionsBuilder.UseSqlServer("Data Source = DESKTOP-JB6Q0DG\\MSSSQLSERVER15; initial catalog = Chapter; Integrated Security = true");
+            }
+        }
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Livro> Livros { get; set; }
+
+    }
+}
---
+++ ChapterSenai/Repositories/LivroRepository.cs
@@ -0,0 +1,23 @@
+﻿using ChapterSenai.Contexts;
+using ChapterSenai.Models;
+using System.Linq;
+
+namespace ChapterSenai.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly Sqlcontext _context;
+        public LivroRepository(Sqlcontext context)
+        {
+            _context = context;
+
+        }
+
+        public List<Livro> Listar()
+        {
+
+            return _context.Livros.ToList();
+        }
+
+    }
+}
